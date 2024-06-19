6c6e43347146da970777502e72e8fdb27fa56450
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-19 12:31:06 +0900

    Create create table.sql

---
+++ School/06.19 SQL/member_tbl_02/create table.sql
@@ -0,0 +1,9 @@
+create table member_tbl_02 (
+	custno number(6) primary key,
+	custname varchar2(20),
+	phone varchar2(13),
+	address varchar2(60),
+	joindate date,
+	grade char(1),
+	city char(2)
+);
