d523b89b43608e1a8d1b8756b6ab31868d66b5de
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-19 12:48:24 +0900

    Create insert into.sql

---
+++ School/06.19 SQL/tablemoney_tbl_02/insert into.sql
@@ -0,0 +1,10 @@
+insert into money_tbl_02 values(100001, 20160001, 500, 5, 2500, 'A001', '20160101');
+insert into money_tbl_02 values(100001, 20160002, 1000, 4, 4000, 'A002', '20160101');
+insert into money_tbl_02 values(100001, 20160003, 500, 3, 1500, 'A008', '20160101');
+insert into money_tbl_02 values(100002, 20160004, 2000, 1, 2000, 'A004', '20160102');
+insert into money_tbl_02 values(100002, 20160005, 500, 1, 500, 'A001', '20160103');
+insert into money_tbl_02 values(100003, 20160006, 1500, 2, 3000, 'A003', '20160103');
+insert into money_tbl_02 values(100004, 20160007, 500, 2, 1000, 'A001', '20160104');
+insert into money_tbl_02 values(100004, 20160008, 300, 1, 300, 'A005', '20160104');
+insert into money_tbl_02 values(100004, 20160009, 600, 1, 600, 'A006', '20160104');
+insert into money_tbl_02 values(100004, 20160010, 3000, 1, 3000, 'A007', '20160106');
