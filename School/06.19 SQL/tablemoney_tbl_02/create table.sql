7c89ebfd5e177246958361342ab23c06a6e1d2d7
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-06-19 12:31:55 +0900

    Create create table.sql

---
+++ School/06.19 SQL/tablemoney_tbl_02/create table.sql
@@ -0,0 +1,9 @@
+create tablemoney_tbl_02 (
+	custno number(6) primary key,
+	salenol number(8) primary key,
+	pcost number(8),
+	amount number(4),
+	price number(8),
+	pcode varchar2(4),
+	sdate date
+);
