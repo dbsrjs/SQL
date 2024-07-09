758c4705a9f11a5c47cb5a3122877bdbb1e7c10e
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-09 14:31:58 +0900

    Update Delete.java

--- School/jdbc/Delete.java
+++ School/jdbc/Delete.java
@@ -4,6 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.Scanner;
 
 public class Delete {
 	public static void main(String[] args) throws Exception {
@@ -18,12 +19,34 @@ public class Delete {
 		
 		ResultSet rs = pstmt.executeQuery();
 		
+		Scanner sc = new Scanner(System.in);
+		int cnt = 0;
+		
 		//1. pc_menu 꺼내오기
 		while(rs.next()) {
-			System.out.println(rs.getString(1));
+			cnt++;
+			System.out.printf("%d.%-6s\t", cnt, rs.getString(1));
+			
+			if(cnt % 5 == 0)
+				System.out.println();
 		}
+		
+		System.out.println();
+		
 		//2. 삭제할 메뉴 선택받기
+		System.out.print("삭제할 메뉴를 선택하세요. > ");
+		int choice = sc.nextInt();
 		
 		//3. 삭제 발사버튼 만들기
+		String 쿼리문2 = "delete from pc_menu where menu_no = ? ";
+						
+		pstmt = con.prepareStatement(쿼리문2);
+		
+		pstmt.setInt(1, choice);
+		
+		int result = pstmt.executeUpdate();
+		if(result == 1) {
+			System.out.println(choice + "번 정상적으로 삭제되었습니다.");
+		}
 	}
 }
