bc7dba8063229621feab91a93a77e32fd90642ef
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-11 11:02:33 +0900

    Create insert_into.sql

---
+++ School/07.11 master/ho_master/insert_into.sql
@@ -0,0 +1,9 @@
+insert into ho_master values(10, 100, 1999);
+insert into ho_master values(20, 2000, 2999);
+insert into ho_master values(30, 3000, 3999);
+insert into ho_master values(40, 4000, 4999);
+insert into ho_master values(50, 5000, 5999);
+insert into ho_master values(60, 6000, 6999);
+insert into ho_master values(70, 7000, 7999);
+insert into ho_master values(80, 8000, 8999);
+insert into ho_master values(90, 9000, 9999);
