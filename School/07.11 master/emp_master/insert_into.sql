f3db17dccd16dc20f41db682a206049bc2ad9ac0
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-07-11 11:01:53 +0900

    Create insert_into.sql

---
+++ School/07.11 master/emp_master/insert_into.sql
@@ -0,0 +1,14 @@
+insert into emp_master values(101, '이기호', '10', 10, 1000);
+insert into emp_master values(111, '정진기', '10', 10, 1500);
+insert into emp_master values(121, '남만열', '20', 10, 1200);
+insert into emp_master values(122, '박선영', '20', 20, 2700);
+insert into emp_master values(123, '박순기', '20', 20, 2500);
+insert into emp_master values(124, '최석금', '30', 20, 2400);
+insert into emp_master values(222, '김한나', '10', 30, 3500);
+insert into emp_master values(223, '정선희', '10', 30, 3400);
+insert into emp_master values(224, '김민석', '30', 30, 3900);
+insert into emp_master values(700, '우민주', '20', null, null);
+insert into emp_master values(701, '황남도', '10', null, null);
+
+insert into emp_master values(900, '우민주', '20', null, null);
+insert into emp_master values(901, '황남도', '10', null, null);
