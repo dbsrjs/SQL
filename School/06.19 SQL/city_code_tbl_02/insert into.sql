7d5afdb01b1876f54a5c2ee2fd83653a699c9083
xyl_t9 <124150775+dbsrjs@users.noreply.github.com>
2024-09-13 11:53:19 +0900

    Create insert into.sql

---
+++ School/06.19 SQL/city_code_tbl_02/insert into.sql
@@ -0,0 +1,9 @@
+insert into city_code_tbl_02 values('10', '서울', sysdate);
+insert into city_code_tbl_02 values('20', '대전', sysdate);
+insert into city_code_tbl_02 values('30', '대구', sysdate);
+insert into city_code_tbl_02 values('40', '광주', sysdate);
+insert into city_code_tbl_02 values('50', '성남', sysdate);
+insert into city_code_tbl_02 values('60', '수원', sysdate);
+insert into city_code_tbl_02 values('70', '분당', sysdate);
+insert into city_code_tbl_02 values('80', '용인', sysdate);
+insert into city_code_tbl_02 values('90', '과천', sysdate);
